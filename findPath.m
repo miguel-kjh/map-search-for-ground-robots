d6d30805e61fdb09b7a002ceb2d4439efe658894
miguel-kjh <miguelangel.kjh@gmail.com>
2021-03-12 14:40:35 +0000

    branching and bound algorithm completed!

--- findPath.m
+++ findPath.m
@@ -1,6 +1,6 @@
 % TODO: check if child point is not in list
 
-function [path] = findPath(costMap, start, final)
+function [path, cost] = findPath(costMap, start, final)
     worlSize = size(costMap,2);
     listDimension = worlSize*worlSize;
     
@@ -21,7 +21,7 @@ function [path] = findPath(costMap, start, final)
             x = nearestNeighbors(i,1);
             y = nearestNeighbors(i,2);
             
-            if (x > 0 && y > 0) && (x <= worlSize && y <= worlSize) && costMap(y,x) > 1
+            if (x > 0 && y > 0) && (x <= worlSize && y <= worlSize) && costMap(y,x) > 1 && ~searchPoint(list, [x,y])
                 list(finalListIndex,1) = x;
                 list(finalListIndex,2) = y;
                 list(finalListIndex,3) = costMap(y,x) + evalPoint(3);
@@ -33,7 +33,27 @@ function [path] = findPath(costMap, start, final)
         listIndex = listIndex + 1;
         pointToEvaluate = list(listIndex:finalListIndex,:);
         sortrows(pointToEvaluate,3);
-        evalPoint = pointToEvaluate(1,:)
+        evalPoint = pointToEvaluate(1,:);
     end
     path = evalPoint;
+    fatherIndex = evalPoint(4);
+    cost = evalPoint(3);
+    
+    while(fatherIndex ~= 0)
+        path = [path; list(fatherIndex,:)];
+        fatherIndex = list(fatherIndex,4);
+    end
+    
 end
+
+
+function [isPoint] = searchPoint(list, point)
+    for i = 1:length(list)
+        if list(i,1) == point(1) && list(i,2) == point(2)
+            isPoint = true;
+            return
+        end
+    end
+    isPoint = false;
+end
+
--- getRobotPath.m
+++ getRobotPath.m
@@ -8,8 +8,9 @@ grid = readtable(mapResultFile);
 
 grid = grid{:,:};
 grid(grid~=0) = 1;
-start = [15 14];
-final = [2, 2];
+start = [18, 16];
+final = [13, 2];
+heuritic = 'ecludian';
 
 figure(1);
 grid(start(2), start(1)) = 2;
@@ -22,9 +23,17 @@ try
     costMap = createCostMap(grid, start, final);
     figure(2);
     pcolor(costMap);
+    [path,cost] = findPath(costMap, start, final);
+
+    figure(3);
+    for i = 1:length(path)
+        grid(path(i,2), path(i,1)) = -2;
+    end
+    pcolor(grid);
+    fprintf('B&B-%s cost: %i\n',heuritic, cost);
 catch exception
     disp("map points not available");
+    throw(exception);
 end
 
-path = findPath(costMap, start, final)
 
